84f9c8e453ec3a48bff5e207feb76a6088217a0c
ashishkanere <ashishkanere@gmail.com>
2024-09-03 12:18:00 +0530

    lecture 10 complete

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
 
 
+with open('config.json', 'r') as c:
+    #print(json.load(c))
+    params = json.load(c)["params"]
+
+local_server =params['local_server']
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/codingthunder'
+if(local_server):
+    app.config['SQLALCHEMY_DATABASE_URI'] =params['local_uri']
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = params['prod_uri']
+    
+
 db = SQLAlchemy(app)
 
 
@@ -19,11 +31,11 @@ class Contacts(db.Model):
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', params=params)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', params=params)
 
 # @app.route("/contact")
 # def contact():
@@ -32,7 +44,7 @@ def about():
 
 @app.route("/post")
 def post():
-    return render_template('post.html')
+    return render_template('post.html',  params=params)
 
 
 @app.route("/contact", methods ={'GET', 'POST'})
@@ -52,7 +64,7 @@ def contact():
 
 
 
-    return render_template('contact.html')
+    return render_template('contact.html', params=params)
 
 
 app.run(debug=True)
